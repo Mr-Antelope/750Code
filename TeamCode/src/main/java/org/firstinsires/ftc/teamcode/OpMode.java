b578233ac830c8480058b293c370f09be3a014c5
OmegaCodeing <58758022+OmegaCodeing@users.noreply.github.com>
2019-12-10 18:51:36 -0800

    Add files via upload

---
+++ TeamCode/src/main/java/org/firstinsires/ftc/teamcode/OpMode.java
@@ -0,0 +1,12 @@
+package org.firstinsires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class OpMode extends LinearOpMode
+{
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+
+    }
+}
